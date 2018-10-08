e10aef91dd7be49d6b4eaa864cb2db46f2d373d0
Tarang <tsgoten@gmail.com>
2018-10-08 12:47:42 -0400

    Implementing camera in progress

--- Roster Scanner/ViewController.swift
+++ Roster Scanner/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var captureSession = AVCaptureSession()
+    
+    var backFacingCamera: AVCaptureDevice?
+    var frontFacingCamera: AVCaptureDevice?
+    var currentDevice: AVCaptureDevice?
+    
+    var stillImageOutput: AVCapturePhotoOutput?
+    var stillImage: UIImage?
+    
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+        if let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+            backFacingCamera = device
+        }
+        
+        currentDevice = backFacingCamera
+        
+        stillImageOutput = AVCapturePhotoOutput()
+        
+        
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentDevice!)
+            
+            captureSession.addInput(captureDeviceInput)
+            captureSession.addOutput(stillImageOutput!)
+            
+            cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            view.layer.addSublayer(cameraPreviewLayer!)
+            cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            cameraPreviewLayer?.frame = view.layer.frame
+        } catch let error {
+            print (error)
+        }
     }
-
-
+    
 }
 
