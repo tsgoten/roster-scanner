f1599781d87344f32c4a70ae523211022e5ef9d9
Tarang <tsgoten@gmail.com>
2018-10-16 11:47:06 -0400

    Created Camera controller file

---
+++ Roster Scanner/CameraController.swift
@@ -0,0 +1,13 @@
+//
+//  CameraController.swift
+//  Roster Scanner
+//
+//  Created by Tarang Srivastava on 10/16/18.
+//  Copyright © 2018 Tarang Srivastava. All rights reserved.
+//
+
+import AVFoundation
+
+class CameraController {
+    
+}
